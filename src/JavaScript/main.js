9828bd931293175bc105ff7600a62a7291194dbd
Nikita Kuchko <kuchko.n.study@gmail.com>
2020-05-02 17:35:49 +0300

    add price-block for desktop and mobile

--- src/JavaScript/main.js
+++ src/JavaScript/main.js
@@ -1,2 +1,28 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        centerPadding: '30px',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+            }         
+        ]
+    });
+});
+
 'use strict';
 const button = document.querySelector('button');
